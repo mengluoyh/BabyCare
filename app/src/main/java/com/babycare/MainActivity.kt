ef2672c62f18811f9b92713359582caa9b808fa3
mengluoyh <155011861+mengluoyh@users.noreply.github.com>
2026-06-27 23:13:15 +0800

    Add files via upload

---
+++ app/src/main/java/com/babycare/MainActivity.kt
@@ -0,0 +1,39 @@
+// BabyCare/app/src/main/java/com/example/babycare/MainActivity.kt
+package com.babycare
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.babycare.databinding.ActivityMainBinding
+import com.babycare.ui.TimerFragment
+import com.babycare.ui.FeedingRecordsFragment
+import com.babycare.ui.ExcreteRecordsFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loadFragment(TimerFragment())
+        }
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_timer -> loadFragment(TimerFragment())
+                R.id.nav_feeding -> loadFragment(FeedingRecordsFragment())
+                R.id.nav_excrete -> loadFragment(ExcreteRecordsFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
