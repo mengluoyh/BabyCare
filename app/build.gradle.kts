453a46ed26256d3f89024c53ff1096791ab71104
mengluoyh <155011861+mengluoyh@users.noreply.github.com>
2026-06-28 10:55:03 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.babycare"   // 建议与 namespace 一致
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 1001
+        versionName = "1.0.0.1"
     }
 
     buildFeatures {
