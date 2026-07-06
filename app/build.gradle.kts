2f846b412e9a01049166a772ef8bbc8804714147
menglouyh <321008028@qq.com>
2026-07-06 00:23:48 +0000

    v2.0.70 版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.babycare"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2060
-        versionName = "2.0.60"
+        versionCode = 2070
+        versionName = "2.0.70"
     }
 
     buildFeatures {
